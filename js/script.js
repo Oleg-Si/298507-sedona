9a56df317de7c8ac5cb382295b03e4f0e6354645
S-Oleg-M <xwx18@mail.ru>
2017-02-11 09:37:16 +0300

    JS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 var openPopup = document.querySelector(".open_modal");
 var popup = document.querySelector(".modal");
 
+
+popup.classList.add("modal_show");
+
+
 openPopup.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.toggle("modal_show");
     popup.classList.toggle("fadeInDownBig");
-    
 });
