14e53e142158c1588cfacd90c5aee59c2bca12ce
S-Oleg-M <xwx18@mail.ru>
2017-02-11 10:06:21 +0300

    JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-var openPopup = document.querySelector(".open_modal");
+var btnOpenPopup = document.querySelector(".open_modal");
 var popup = document.querySelector(".modal");
-
+var form = popup.querySelector("form");
+var dateEntry = popup.getElementById("entry");
+var dateExit = popup.getElementById("exit");
 
 popup.classList.add("modal_show");
 
-
-openPopup.addEventListener("click", function(event) {
+btnOpenPopup.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.toggle("modal_show");
     popup.classList.toggle("fadeInDownBig");
+});
+
+form.addEventListener("sumbit", function(event) {
+    if (!dateEntry.value || !dateExit.value) {
+        event.preventDefault();
+        popup.classList.remove("modal_error");
+        popup.classList.add("modal_error");
+    }
 });
