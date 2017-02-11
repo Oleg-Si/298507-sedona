faca18093f68798a0829394bc09126eadc91abf7
S-Oleg-M <xwx18@mail.ru>
2017-02-11 12:34:05 +0300

    JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var btnOpenPopup = document.querySelector(".open_modal");
 var popup = document.querySelector(".modal");
 var form = popup.querySelector("form");
-var dateEntry = popup.getElementById("entry");
-var dateExit = popup.getElementById("exit");
+var dateEntry = document.getElementById("entry");
+var dateExit = document.getElementById("exit");
 
 popup.classList.add("modal_show");
 
@@ -12,7 +12,7 @@ btnOpenPopup.addEventListener("click", function(event) {
     popup.classList.toggle("fadeInDownBig");
 });
 
-form.addEventListener("sumbit", function(event) {
+form.addEventListener("submit", function(event) {
     if (!dateEntry.value || !dateExit.value) {
         event.preventDefault();
         popup.classList.remove("modal_error");
