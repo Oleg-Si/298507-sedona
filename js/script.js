3a2fd3d0bd1a02cc685e9059604b928087705da6
S-Oleg-M <xwx18@mail.ru>
2017-02-11 17:50:51 +0300

    Fin

--- js/script.js
+++ js/script.js
@@ -10,12 +10,23 @@ btnOpenPopup.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.toggle("modal_show");
     popup.classList.toggle("fadeInDownBig");
+    popup.classList.remove("modal_error");
 });
 
 form.addEventListener("submit", function(event) {
     if (!dateEntry.value || !dateExit.value) {
         event.preventDefault();
         popup.classList.remove("modal_error");
+        popup.offsetWidth = popup.offsetWidth;
         popup.classList.add("modal_error");
+    }
+});
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (!popup.classList.contains("modal_show")) {
+            popup.classList.add("modal_show");
+            popup.classList.remove("modal_error");
+        }
     }
 });
